ce7c71c5119617f6e9961057a0ca3f487a7b5e75
jacobreesgit <jacobrees@me.com>
2025-05-30 18:06:55 +0100

    Stage One

---
+++ Music Memory/musicmemory_app.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import MusicKit
+import MediaPlayer
+
+@main
+struct MusicMemoryApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
